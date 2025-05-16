122f1f07da7b13b21fef77c8cf2e387c097e3994
SwanHub <jdprince555@gmail.com>
2025-05-16 02:42:06 -0400

    okay it is in a good state

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button_Generic } from "@/components/shared/Button_Generic";
-import { createVectorEmbOfImage, testEdgeFunction } from "../_api/api";
+import { createVectorEmbOfImage } from "../_api/api";
 import { Input_Text } from "@/components/shared/Input_Text";
 import { useState } from "react";
 import {
@@ -64,7 +64,7 @@ export default function Leaderboard() {
       </div>
 
       {/* part 1: create new image */}
-      {/* <div className="flex flex-col">
+      <div className="flex flex-col">
         <div className="flex w-full pb-6">
           <Input_Text
             label="Initial image url"
@@ -78,10 +78,10 @@ export default function Leaderboard() {
           label="Save image to our bucket"
           onClick={createNewImage}
         />
-      </div> */}
+      </div>
 
       {/* part 2: create vector embedding of said image */}
-      {/* {supabaseImageUrl && (
+      {supabaseImageUrl && (
         <div>
           <Button_Generic
             label="Get and save image embedding"
@@ -89,10 +89,10 @@ export default function Leaderboard() {
           />
           <p>Embedding, printed (to check): {JSON.stringify(embedding)}</p>
         </div>
-      )} */}
+      )}
 
       {/* part 3: given a vector embedding and imageUrl, insert value to our "base_comparisons" table */}
-      {/* {embedding && (
+      {embedding && (
         <div>
           <div className="flex w-full pb-6">
             <Input_Text
@@ -115,7 +115,7 @@ export default function Leaderboard() {
             onClick={saveBaseComparison}
           />
         </div>
-      )} */}
+      )}
     </div>
   );
 }
