ef758fed63c6ef32ae97554ea33ef4d66d178139
SwanHub <jdprince555@gmail.com>
2025-05-28 17:59:04 -0400

    moving and grooving

--- src/lib/api/testzone.ts
+++ src/lib/api/testzone.ts
@@ -1,19 +1,3 @@
-export const compareToCentroid = async (imageUrl: string): Promise<any> => {
-  const response = await fetch(
-    "http://127.0.0.1:8000/general_chalamet_similarity",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ image: imageUrl }),
-    }
-  );
-
-  const result = await response.json();
-  return result;
-};
-
 export const calculateTextClipComparison = async (
   imageUrl: string
 ): Promise<number[] | undefined> => {
