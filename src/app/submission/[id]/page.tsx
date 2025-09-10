79d57055e04bc0277c6b414dbd413eb70c65a4f7
SwanHub <jdprince555@gmail.com>
2025-09-10 13:02:08 -0400

    adding how it works to submission page

--- src/app/submission/[id]/page.tsx
+++ src/app/submission/[id]/page.tsx
@@ -10,6 +10,7 @@ import { Comparison } from "./_components/Comparison";
 import { ShareButtons } from "./_components/ShareButtons";
 import { Dopplegangers } from "./_components/Dopplegangers";
 import { Report } from "./_components/Report";
+import { Calculations } from "./_components/Calculations";
 
 export default function SubmissionPage({
   params,
@@ -43,6 +44,7 @@ export default function SubmissionPage({
           id={data.submission.id}
           name={data.submission.name ?? "Your"}
         />
+        <Calculations data={data} />
       </div>
     </div>
   );
